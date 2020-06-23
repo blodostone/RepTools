ef45eed7062287f895a1f878b4504690094aa011
bloodstone <maks.khokholov.99@gmail.com>
2020-06-23 19:33:58 +0300

    nvb

---
+++ lesson6/task2/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'boundle.js'
+    },
+    module: {
+        rules: [{
+            test: /.js$/,
+            use: ['babel-loader']
+        }],
+    }
+}
