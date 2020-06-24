e7899506ad0ebbc5f1d0d1d449e9a6a0efa4ef76
bloodstone <maks.khokholov.99@gmail.com>
2020-06-24 03:36:57 +0300

    hfgb

---
+++ lesson9/task1/src/index.js
@@ -0,0 +1,22 @@
+import printProfile from './renderProfile';
+
+const userData = {
+    name: 'Tom',
+    age: 17,
+};
+
+const profile = {
+    ...userData,
+    company: 'Gromcode',
+};
+
+printProfile(profile);
+
+console.log(profile);
+
+
+const number = 17;
+
+if (num == 17) {
+    alert('hi')
+}
